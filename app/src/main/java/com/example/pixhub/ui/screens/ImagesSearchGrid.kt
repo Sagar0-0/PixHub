99203cfbfcaf1d9063f97e88ab025c010f90f6d3
Sagar <sagar.0dev@gmail.com>
2023-03-07 21:18:30 +0530

    added custom indicator

--- app/src/main/java/com/example/pixhub/ui/screens/ImagesSearchGrid.kt
+++ app/src/main/java/com/example/pixhub/ui/screens/ImagesSearchGrid.kt
@@ -2,17 +2,23 @@ package com.example.pixhub.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.*
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import com.example.pixhub.R
 import com.example.pixhub.ui.data.ImageViewModel
@@ -20,10 +26,7 @@ import com.example.pixhub.utils.ApiType
 import com.example.pixhub.utils.ImageTabItem
 import com.example.pixhub.utils.customDrawerShape
 import com.example.pixhub.utils.toPx
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 import kotlinx.coroutines.launch
 
 @SuppressLint("CoroutineCreationDuringComposition")
@@ -34,22 +37,19 @@ import kotlinx.coroutines.launch
 fun ImageSearchGrid(navController: NavHostController, imageViewModel: ImageViewModel) {
     val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState()
-    coroutineScope.launch { imageViewModel.searchPexelsImage() }
-    coroutineScope.launch { imageViewModel.searchPixabayImage() }
-
 
     val tabRowItems = listOf(
         ImageTabItem(
             apiType = ApiType.UNSPLASH,
-            screen = { Unsplash(imageViewModel = imageViewModel, navController = navController) }
+            screen = { Profile() }
         ),
         ImageTabItem(
             apiType = ApiType.PEXELS,
-            screen = { Pexels(imageViewModel = imageViewModel, navController = navController) }
+            screen = { Settings() }
         ),
         ImageTabItem(
             apiType = ApiType.PIXABAY,
-            screen = { Pixabay(imageViewModel = imageViewModel, navController = navController) }
+            screen = { History() }
         )
     )
 
@@ -60,10 +60,7 @@ fun ImageSearchGrid(navController: NavHostController, imageViewModel: ImageViewM
             backgroundColor = Color.Transparent,
             selectedTabIndex = pagerState.currentPage,
             indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
-                    color = MaterialTheme.colors.secondary
-                )
+                CustomIndicator(tabPositions = tabPositions, pagerState = pagerState)
             }
         ) {
             tabRowItems.forEachIndexed { index, item ->
@@ -86,74 +83,42 @@ fun ImageSearchGrid(navController: NavHostController, imageViewModel: ImageViewM
     }
 }
 
-
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun ApiHeader(selectedApi: ApiType, onClick: (ApiType) -> Unit) {
-
-    val getBorderStroke: (Boolean) -> BorderStroke? = { selected ->
-        if (selected) {
-            BorderStroke(1.dp, Color.Cyan.copy(0.8f))
-        } else {
-            null
-        }
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 4.dp, end = 4.dp, top = 4.dp)
+private fun CustomIndicator(tabPositions: List<TabPosition>, pagerState: PagerState) {
+    val transition = updateTransition(pagerState.currentPage, label = "")
+    val indicatorStart by transition.animateDp(
+        transitionSpec = {
+            if (initialState < targetState) {
+                spring(dampingRatio = 1f, stiffness = 50f)
+            } else {
+                spring(dampingRatio = 1f, stiffness = 100f)
+            }
+        }, label = ""
     ) {
-        Card(
-            border = getBorderStroke(selectedApi == ApiType.UNSPLASH),
-            modifier = Modifier
-                .padding(horizontal = 3.dp)
-                .weight(1f)
-                .height(40.dp)
-                .background(color = Color.White.copy(0.8f), shape = RoundedCornerShape(15.dp))
-                .clickable { onClick(ApiType.UNSPLASH) }
-        ) {
-            Image(
-                modifier = Modifier.padding(12.dp),
-                painter = painterResource(id = R.drawable.unsplash_logo),
-                contentDescription = "Unsplash"
-            )
-        }
-        Card(
-            border = getBorderStroke(selectedApi == ApiType.PEXELS),
-            modifier = Modifier
-                .padding(horizontal = 3.dp)
-                .weight(1f)
-                .height(40.dp)
-                .background(color = Color.White.copy(0.8f), shape = RoundedCornerShape(15.dp))
-                .clickable { onClick(ApiType.PEXELS) }
-        ) {
-            Image(
-                modifier = Modifier.padding(3.dp),
-                painter = painterResource(id = R.drawable.pexels_logo),
-                contentDescription = "Pexels"
-            )
-        }
-        Card(
-            border = getBorderStroke(selectedApi == ApiType.PIXABAY),
-            modifier = Modifier
-                .padding(horizontal = 3.dp)
-                .weight(1f)
-                .height(40.dp)
-                .background(color = Color.Green.copy(0.8f), shape = RoundedCornerShape(15.dp))
-                .clickable { onClick(ApiType.PIXABAY) }
-        ) {
-            Image(
-                modifier = Modifier.padding(12.dp),
-                painter = painterResource(id = R.drawable.pixabay_logo),
-                contentDescription = "Pixabay"
-            )
-        }
+        tabPositions[it].left
     }
-}
 
-@Composable
-@Preview
-fun ApiHeaderPreview() {
-    ApiHeader(selectedApi = ApiType.UNSPLASH, onClick = {})
+    val indicatorEnd by transition.animateDp(
+        transitionSpec = {
+            if (initialState < targetState) {
+                spring(dampingRatio = 1f, stiffness = 100f)
+            } else {
+                spring(dampingRatio = 1f, stiffness = 50f)
+            }
+        }, label = ""
+    ) {
+        tabPositions[it].right
+    }
+
+    Box(
+        Modifier
+            .offset(x = indicatorStart)
+            .wrapContentSize(align = Alignment.BottomStart)
+            .width(indicatorEnd - indicatorStart)
+            .fillMaxSize()
+            .border(BorderStroke(2.dp, Color(0xFF00FFCC)), RoundedCornerShape(50))
+            .padding(5.dp)
+    )
 }
 
